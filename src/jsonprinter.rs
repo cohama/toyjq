c1ecbd8cc08c127ee2f5040ed4c2ca7421151fa8
cohama <cohama@live.jp>
2017-05-07 00:05:37 +0900

    Implement JSON printer

---
+++ src/jsonprinter.rs
@@ -0,0 +1,68 @@
+use super::jsonparser::*;
+use super::*;
+
+use std::collections::HashMap;
+
+const INDENT_DEPTH: i32 = 2;
+
+pub fn print_json(json: &Json, width: i32) -> String {
+    Doc::new(vec![json_to_doc_elem(json)]).pretty(width)
+}
+
+fn json_to_doc_elem(json: &Json) -> DocElem {
+    match *json {
+        Json::JNumber(v) => text(format!("{}", v)),
+        Json::JString(s) => text(s.to_string()),
+        Json::JBool(true) => literal("true"),
+        Json::JBool(false) => literal("false"),
+        Json::JNull => literal("null"),
+        Json::JArray(ref jsons) => json_vec_to_flatable_doc_elem(jsons),
+        Json::JObject(ref obj) => json_object_to_flatable_doc_elem(obj)
+    }
+}
+
+fn json_vec_to_flatable_doc_elem(jsons: &Vec<Json>) -> DocElem {
+    if jsons.is_empty() {
+        literal("[]")
+    } else {
+        let mut it = jsons.iter();
+        let mut ret = vec![literal("["), newline(INDENT_DEPTH)];
+        ret.push(json_to_doc_elem(it.next().unwrap()));
+        while let Some(j) = it.next() {
+            ret.push(literal(","));
+            ret.push(newline(0));
+            ret.push(json_to_doc_elem(j));
+        }
+        ret.push(newline(-2));
+        ret.push(literal("]"));
+        flatable(ret)
+    }
+}
+
+fn json_object_to_flatable_doc_elem(obj: &HashMap<&str, Json>) -> DocElem {
+    if obj.is_empty() {
+        literal("{}")
+    } else {
+        let mut it = obj.iter();
+        let mut ret = vec![literal("{"), newline(INDENT_DEPTH)];
+        let kv0 = it.next().unwrap();
+        ret.append(&mut json_keyvalue_to_doc_elems(kv0));
+        while let Some(kv) = it.next() {
+            ret.push(literal(","));
+            ret.push(newline(0));
+            ret.append(&mut json_keyvalue_to_doc_elems(kv));
+        }
+        ret.push(newline(-2));
+        ret.push(literal("}"));
+        flatable(ret)
+    }
+}
+
+fn json_keyvalue_to_doc_elems(keyvalue: (&&str, &Json)) -> Vec<DocElem> {
+    let (k, v) = keyvalue;
+    vec![
+        text(k.to_string()),
+        literal(": "),
+        json_to_doc_elem(v)
+    ]
+}
--- src/lib.rs
+++ src/lib.rs
@@ -3,3 +3,8 @@ pub use parsercombinator::*;
 
 pub mod prettyprinter;
 pub use prettyprinter::*;
+
+pub mod jsonprinter;
+pub use jsonprinter::*;
+
+pub mod jsonparser;
