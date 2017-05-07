b4455ca1b45109e9d9abbdd7ee5579643f092fd2
cohama <cohama@live.jp>
2017-05-07 16:37:23 +0900

    Implement pretty-print JSON input from stdin

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,27 @@
 extern crate toyjq;
 
 use toyjq::*;
+use toyjq::jsonparser::*;
+
+use std::io;
+use std::io::*;
 
 fn main() {
-    println!("{:?}", string("foo").parse("fooo").unwrap());
+    interact(|s| {
+        let json = parse_json().parse(s).unwrap();
+        print_json(&json, 80)
+    }).unwrap_or_else(|e| {
+        println!("ERROR");
+        println!("{:?}", e);
+    })
+}
+
+fn interact<F>(f: F) -> io::Result<()>
+    where F: FnOnce(&str) -> String
+{
+    let mut input = String::new();
+    io::stdin().read_to_string(&mut input)?;
+    println!("{}", f(input.as_ref()));
+
+    Ok(())
 }
