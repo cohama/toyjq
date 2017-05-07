6d0193b593a2c917e4f6003ddbe4d458aadb9475
cohama <cohama@live.jp>
2017-05-07 18:24:35 +0900

    Refactor main

--- src/main.rs
+++ src/main.rs
@@ -3,24 +3,33 @@ extern crate toyjq;
 use toyjq::Json;
 
 use std::io;
-use std::io::*;
+use std::io::{Read};
 
 fn main() {
     interact(|s| {
-        let json = Json::from_str(s).unwrap();
-        json.pretty_print(80)
+        let json = Json::from_str(s).map_err(ToyjqError::ParseError)?;
+        Ok(json.pretty_print(80))
     }).unwrap_or_else(|e| {
         println!("ERROR");
         println!("{:?}", e);
     })
 }
 
-fn interact<F>(f: F) -> io::Result<()>
-    where F: FnOnce(&str) -> String
+#[derive(Debug)]
+enum ToyjqError {
+    IoError(io::Error),
+    ParseError(toyjq::parsercombinator::ParseError)
+}
+
+type ToyjqResult<T> = std::result::Result<T, ToyjqError>;
+
+fn interact<F>(f: F) -> ToyjqResult<()>
+    where F: FnOnce(&str) -> ToyjqResult<String>
 {
     let mut input = String::new();
-    io::stdin().read_to_string(&mut input)?;
-    println!("{}", f(input.as_ref()));
+    io::stdin().read_to_string(&mut input).map_err(ToyjqError::IoError)?;
+    let s = f(input.as_ref())?;
+    println!("{}", s);
 
     Ok(())
 }
